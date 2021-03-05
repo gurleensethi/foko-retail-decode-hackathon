f68e22205e3026bb63217e20f37b8175ab4fed65
Gurleen Sethi <sarusethi@rocketmail.com>
2021-03-04 21:17:43 -0500

    feat: add baseweb.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import './App.css';
+import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
-import CustomerHome from './components/customerHome';
-import RetailerHome from './components/retailerHome';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import CustomerHome from "./components/customerHome";
+import RetailerHome from "./components/retailerHome";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider } from "baseui";
+
+const engine = new Styletron();
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <NavLink to="/customer">Customer</NavLink> | <NavLink to="retailer">Retailer</NavLink>
-        </Route>
-        <Route path="/customer">
-          <CustomerHome />
-        </Route>
-        <Route path="/retailer">
-          <RetailerHome />
-        </Route>
-      </Switch>
-    </Router>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <NavLink to="/customer">Customer</NavLink> |{" "}
+              <NavLink to="retailer">Retailer</NavLink>
+            </Route>
+            <Route path="/customer">
+              <CustomerHome />
+            </Route>
+            <Route path="/retailer">
+              <RetailerHome />
+            </Route>
+          </Switch>
+        </Router>
+      </BaseProvider>
+    </StyletronProvider>
   );
 }
 
--- src/components/customerHome.jsx
+++ src/components/customerHome.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useStyletron } from "baseui";
+import { Button } from "baseui/button";
 
 const CustomerHome = () => {
-    return (
-        <div>
-            <h2>This is the homepage for customers.</h2>
-        </div>
-    )
-}
+  const [css] = useStyletron();
+
+  return (
+    <div>
+      <h2>This is the homepage for customers.</h2>
+      <Button className={css({ margin: "10px" })}>
+        I am a button from Uber
+      </Button>
+    </div>
+  );
+};
 
 export default CustomerHome;
