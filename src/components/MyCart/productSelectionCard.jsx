f04320a7283003de31f26be9e80b4409cc804896
Bill Zhang <bill.nonbusiness@gmail.com>
2021-03-05 17:34:50 -0500

    finialized my cart screen

--- src/components/MyCart/productSelectionCard.jsx
+++ src/components/MyCart/productSelectionCard.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { Card } from 'baseui/card';
-import { Button, KIND, SHAPE, SIZE } from 'baseui/button';
 import { Select, SIZE as SelectSIZE } from "baseui/select";
-import { Trash } from 'react-feather';
+import { ParagraphXSmall,ParagraphSmall } from "baseui/typography";
 
 const options = [
     { label: 1, id: 1 },
@@ -17,7 +16,7 @@ const ProductSelectionCard = (props) => {
 
     const updateQuantity = (newQuantity) => {
         setValue(newQuantity);
-        props.onQuantityChange(props.name, newQuantity[0]?.label || 0);
+        props.onQuantityChange(props.index, newQuantity[0]?.label || 0);
     };
 
     return (
@@ -25,37 +24,51 @@ const ProductSelectionCard = (props) => {
             Root: {
                 style: {
                     borderWidth: 0,
-                    paddingRight: 0
+                    height:90
                 }
-            }
+            },
         }}>
             <div style={{
                 display: 'flex',
                 justifyContent: 'space-around',
-                alignItems: 'center'
+                alignItems: 'center',
+                height: 90,
+                paddingBottom: 20,
+                paddingTop: 10
             }}>
 
+            
             <img style={{
-                width: 60
-            }} src={props.imgUrl} alt='new' />
+                width: 75,
+                height: 75,
+                overflow: 'hidden',
+                borderRadius: 10,
+            }} src={props.imgUrl} alt='{props.imgUrl}' />
             
-            <span style={{ fontWeight: 'bold' }}>{props.name}</span>
-            
-            <Select options={options}
-                value={value}
-                clearable={false}
-                size={SelectSIZE.compact}
-                placeholder={(<span style={{width:20}}></span>)}
-                onChange={params => updateQuantity(params.value)}
-                overrides={{Root:{style:{width:10}}}}
-            />
+            <div style={{flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'space-around'}}>
+                <ParagraphXSmall margin="0px" color="#828282">
+                {props.brand}
+                </ParagraphXSmall>
+                <ParagraphSmall margin="0px">
+                {props.name}
+                </ParagraphSmall>
+                <ParagraphXSmall margin="0px" color="#828282">
+                Item No: {props.itemId}
+                </ParagraphXSmall>
+                <Select options={options}
+                    value={value}
+                    clearable={false}
+                    size={SelectSIZE.compact}
+                    placeholder={(<span style={{width:20}}></span>)}
+                    onChange={params => updateQuantity(params.value)}
+                    overrides={{Root:{style:{width:10}}}}
+                />
+            </div>
             
            <span style={{ width: 30, textAlign: 'center' }}>${props.price * value[0]?.label || 0}</span>
-           
-            <Button kind={KIND.secondary} size={SIZE.mini} shape={SHAPE.pill} style={{marginLeft: 10}}>
-                <Trash size={15} />
-            </Button>
+
             </div>
+            <hr style={{height: 2, borderColor: 'white', color: 'lightgray' }} />
         </Card>
     )
 };
